ea0aa76895c71cc973ccf4179aefe4f83ceb1b6a
anutati <anutati@yahoo.com>
2023-01-11 12:50:33 -0500

    Desktop version

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('.slider').slick({
+    dots:true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/Arrow_left.svg" alt="arrow left"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/Arrow_right.svg" alt="arrow left"></button>',
+  });
+})
