296f58a6528ad5464d6faa5a2764828747d45b48
Anna Kem <annakemik@gmail.com>
2023-02-01 12:05:38 -0500

    Responsive design

--- js/main.js
+++ js/main.js
@@ -4,4 +4,14 @@ $(function(){
     prevArrow: '<button type="button" class="slick-prev"><img src="images/Arrow_left.svg" alt="arrow left"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/Arrow_right.svg" alt="arrow left"></button>',
   });
+
+  $('.nav-btn, .menu a').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
 })
+
+
+
+
+
+
